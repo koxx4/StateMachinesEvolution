fbf891dfaec4cd2ffe50af221d425f2b0fcba5b8
koxx4 <piotr815@protonmail.com>
2021-12-08 19:37:02 +0100

    Added future methods for decoding moore machine

--- src/main/java/org/koxx4/machine/MooreMachine.java
+++ src/main/java/org/koxx4/machine/MooreMachine.java
@@ -6,4 +6,10 @@ public class MooreMachine extends StateMachine<MooreState>{
         super(configuration);
     }
 
+    public static MooreMachine decodeFromEquation(String equation){
+        char[] equationTokens = equation.toCharArray();
+
+        return null;
+    }
+
 }
